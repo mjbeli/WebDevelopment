e7609cc66a7b687ca0328ba9698b88f9f5c85ef9
mjbeli <mjba82@gmail.com>
2020-05-31 12:24:37 +0200

    Se incluye documentación generada con swagger/OpenAPI

--- LibraryWebAPI/LibraryWebAPI/Controllers/LibraryController.cs
+++ LibraryWebAPI/LibraryWebAPI/Controllers/LibraryController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LibraryDAO;
 using LibraryDAO.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace LibraryWebAPI.Controllers
 {
@@ -24,10 +25,42 @@ namespace LibraryWebAPI.Controllers
             _lib = lib;
         }
 
-        [HttpGet("GetByCifiGenre")]
-        public IEnumerable<BookDTO> GetByCifiGenre()
+        /// <summary>
+        /// Endpoint que devuelve los libros de un determinado género.
+        /// </summary>
+        /// <param name="genre">Género que se quiere buscar</param>
+        /// <returns>Una lista en formato JSON con los libros del género buscado</returns>
+        [HttpGet("GetByGenre/{genre}")] // URL as https://<<server>>:<<port>>/Library/GetByGenre/{genre}
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces("application/json")] // Indica que la respuesta es JSON.
+        public IActionResult GetByGenre(string genre)
         {
-            return _lib.GetByGenre("Ci-fi");
+            if (string.IsNullOrWhiteSpace(genre))
+                return BadRequest();
+
+            IEnumerable<BookDTO> listBooks = _lib.GetByGenre(genre);
+            if (listBooks == null || listBooks.Count() == 0)
+                return NotFound();
+
+            return Ok(listBooks);
         }
+
+        /*
+         * We can use type JsonResult to return a Json in an API like this:
+         *  [HttpGet]
+         *  public JsonResult Get()
+         *  {
+         *      return Json(_authorRepository.List());
+         *  }
+         *  
+         *  But in these cases we can't control the response code. In other words
+         *  we can't use --> return NotFount(); or return Ok();
+         *  
+         *  So we use IActionResult and we can return a Json with response code.
+         *  https://docs.microsoft.com/es-es/dotnet/api/microsoft.aspnetcore.mvc.iactionresult?view=aspnetcore-2.1
+        */
+
     }
 }
--- LibraryWebAPI/LibraryWebAPI/Startup.cs
+++ LibraryWebAPI/LibraryWebAPI/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using LibraryDAO;
 using Microsoft.AspNetCore.Builder;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 [assembly: ApiController]
 namespace LibraryWebAPI
@@ -28,27 +31,50 @@ namespace LibraryWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddTransient<ILibraryDAO, LibraryDAO.LibraryDAO>(service => new LibraryDAO.LibraryDAO("mongodb://localhost:27017", "Library", "Books")); // IoC
+
+            // IoC
+            services.AddTransient<ILibraryDAO, LibraryDAO.LibraryDAO>(service => new LibraryDAO.LibraryDAO("mongodb://localhost:27017", "Library", "Books"));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Title = "Library API", 
+                    Version = "v1",
+                    Description = "A simple example ASP.NET Core Web API"
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
-        }
-    }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Library API V1");
+                c.RoutePrefix = "library/help"; // Swagger page will be in 
+            });
+        } // end Configure
+    } // end Starup class
 }
