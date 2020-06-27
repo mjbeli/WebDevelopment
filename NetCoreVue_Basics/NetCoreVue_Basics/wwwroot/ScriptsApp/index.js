579c30cb9a562e30b490be7b0f7dffe60be6b8a5
mjbeli <mjba82@gmail.com>
2020-06-27 12:02:42 +0200

    Simple app with Vue experiments

---
+++ NetCoreVue_Basics/NetCoreVue_Basics/wwwroot/ScriptsApp/index.js
@@ -0,0 +1,23 @@
+﻿Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{todo.text}}</li>'
+})
+
+
+var app = new Vue(
+    {
+        el: '#app',
+        data: {
+            mensaje: 'Hola desde Vue!',
+            groceryList: [{ id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'meat' }]
+        },
+        methods: {
+            reverseMensaje: function () {
+                this.mensaje = this.mensaje.split('').reserse().join('')
+            }
+        }
+
+    }
+);
