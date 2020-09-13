dd503fc544655dc1604e00787a09f8847c608178
mjbeli <mjba82@gmail.com>
2020-09-13 12:27:42 +0200

    initial commit

---
+++ VueJS/08-animations/README.md
@@ -0,0 +1,7 @@
+# 08-animations
+
+```html
+<transition></transition>
+```
+is a tag that provides vue and animate the element inside the tag. We can have several elements inside transition tag but only of them can be show at a time. We cann't animate a list. We can have several div but they have to be exclude between them with v-if or similar.
+
---
+++ VueJS/08-animations/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <button class="btn btn-primary" @click="show=!show">Alert</button>
+                <br><br>
+                <transition>
+                    <dir class="alert alert-info" v-if="show">This is some info</dir>
+                </transition>
+                    
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show: false
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
