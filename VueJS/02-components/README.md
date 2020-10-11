65f8971b654b4bf65521fea7b569d2091a6d2b58
mjbeli <mjba82@gmail.com>
2020-10-11 11:46:07 +0200

    first commit components

---
+++ VueJS/02-components/README.md
@@ -0,0 +1,5 @@
+# 02-components
+
+### 01.01 Introduction
+
+Components are reusable pieces of html with connected data and logic. This pieces can be reuses over the application or over the pages. Each data and logic of a instance it's independent from other intances. A component encapsulate structure, content and logic into small reusable pieces.
---
+++ VueJS/02-components/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      friends: [
+        {
+          id: 'aa',
+          name: 'aa',
+          telephone: '12345',
+          email: 'aa@invent.com'
+          },
+          {
+          id: 'bb',
+          name: 'bb',
+          telephone: '67890',
+          email: 'bb@invent.com'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
